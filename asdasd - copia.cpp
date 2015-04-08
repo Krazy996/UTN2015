ecb888f3fa8f7c672b88a5a580080582e6a7a764
Tibu <braiale@hotmail.com>
2015-04-08 13:44:07 -0300

    Tibu

---
+++ asdasd - copia.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int I; 
+char a;
+main()
+{
+      system("color 0a");
+      for (I=0;I<=20;I+5)
+      printf("%d", &I);
+      system("pause");
+      return 0;
+}
