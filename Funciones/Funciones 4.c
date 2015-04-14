c506b00fdb0262c840e21030c6d71994db9ff734
Tibu <braiale@hotmail.com>
2015-04-14 12:30:37 -0300

    Programas

--- Funciones/Funciones 4.c
+++ Funciones/Funciones 4.c
@@ -54,66 +54,9 @@ int buscar_menor(Pila *origen, Pila *otra, int *men)
 		pasar_a_otra(otra,origen);
 		if (pilavacia(&comparar))
 		{
-			apilar(&menor,desapilar(comparar));
+			apilar(&menor,desapilar(&comparar));
 		}
-		if (tope(&dada)<tope(&comparar))
-		{
-			apilar(&aux, desapilar(&menor));
-			apilar(&menor, desapilar(&comparar));
-		}
-		else
-		{
-			apilar(&aux, desapilar(&dada));
-		}
-	}
-	pasar_a_otra(otra,origen);
-	tope(&menor)=men;
-}	buscar_menor(&origen,&otra);
-	mostrar(&origen);
-	printf("El menor valor de la pila %d",men);
-	system("pause");
-	return(0);
-}
-//Declaracion de funciones...
-//
-void ingresar_elementos(Pila *origen)
-{
-	int op;
-	do
-	{
-		op=0;
-		leer(origen);
-		printf("¿Desea seguir ingresando elementos? (elija la opcion numerica):\n ");
-		printf("1-SI \n 2-NO \n");
-		scanf("%d",&op);
-	}while(op==1);
-}
-//
-//
-void pasar_a_otra(Pila *origen,Pila *otra)
-{
-	while(!pilavacia(origen))
-	{
-		apilar(otra, desapilar(origen));
-	}
-}
-int buscar_menor(Pila *origen, Pila *otra, int *men)
-{
-	int men;
-	Pila menor,comparar,aux;
-	inicpila(&menor);
-	inicpila(&aux);
-	inicpila(&comparar);
-	pasar_a_otra(origen,otra);
-	while(!pilavacia(otra))
-	{
-		tope(&comparar)=tope(otra);
-		pasar_a_otra(otra,origen);
-		if (pilavacia(&comparar))
-		{
-			apilar(&menor,desapilar(comparar));
-		}
-		if (tope(&dada)<tope(&comparar))
+		if (tope(&comparar)<tope(&menor))
 		{
 			apilar(&aux, desapilar(&menor));
 			apilar(&menor, desapilar(&comparar));
