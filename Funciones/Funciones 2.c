6de5a8b5cdcd2cf070c9dae1de3cd456a2d74c43
Tibu <braiale@hotmail.com>
2015-04-14 10:59:23 -0300

    Programas

--- Funciones/Funciones 2.c
+++ Funciones/Funciones 2.c
@@ -5,23 +5,23 @@ void ingresar_elementos(Pila *);
 void pasar_en_orden(Pila *);
 main()
 {
-	Pila elementos,elementos2;
-	inicpila(&elementos2);
-	inicpila(&elementos);
-	ingresar_elementos(&elementos);
-	pasar_en_orden();
+	Pila origen,destino;
+	inicpila(&origen);
+	inicpila(&destino);
+	ingresar_elementos(&origen);
+	pasar_a_destino();
 	system("pause");
 	return(0);
 }
 //Declaracion de funciones...
 //
-void ingresar_elementos(Pila *elementos)
+void ingresar_elementos(Pila *origen)
 {
 	int op;
 	do
 	{
 		op=0;
-		leer(elementos);
+		leer(origen);
 		printf("¿Desea seguir ingresando elementos? (elija la opcion numerica):\n ");
 		printf("1-SI \n 2-NO \n");
 		scanf("%d",&op);
@@ -29,13 +29,12 @@ void ingresar_elementos(Pila *elementos)
 }
 //
 //
-void pasar_en_orden(Pila *elementos *elementos2)
+void pasar_a_destino(Pila *origen, Pila *destino)
 {
-	Pila aux;
-	inicpila (&aux);
-	while(!pilavacia(elementos))
+	while(!pilavacia(origen))
 	{
-		apilar(&aux, desapilar(elementos));
-		apilar(elementos2, desapilar(elementos));
+		apilar(destino, desapilar(origen));
 	}
 }
+//
+//
