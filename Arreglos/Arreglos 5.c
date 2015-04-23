a3ab700604a0e6912e147c909ed38ef9c11d73e1
Tibu <braiale@hotmail.com>
2015-04-23 12:10:50 -0300

    programas

---
+++ Arreglos/Arreglos 5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+	int cant,i;
+	int datos[10];
+	cant=cargar(datos,10);
+	sum=suma(cant,datos);
+	printf("La suma de los elementos del arreglo es de: %d\n",sum );
+}
+/*
+FUNCIONES
+*/
+int cargar(int datos[], int dimension)
+{
+	int i = 0;
+	char control = 's';
+	do {
+        printf("Ingresar número entero: \n");
+		fflush(stdin);
+		scanf("%d",&datos[i]);
+		printf("Desea seguir cargando elementos?:\n s/n \n");
+		fflush(stdin);
+		scanf("%c",&control);
+		i++;
+	} while (i < dimension && control =='s');
+	return i;
+}
+//
+//
