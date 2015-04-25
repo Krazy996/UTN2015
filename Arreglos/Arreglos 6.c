6f99c126a9ddcfa5bb65c4c6ce25fe87de59daf7
Tibu <braiale@hotmail.com>
2015-04-24 22:00:33 -0300

    programas

--- Arreglos/Arreglos 6.c
+++ Arreglos/Arreglos 6.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-main()
+int cargar(int datos[], int dimension);
+void busqueda(int cant, char datos[]);
+int main(int argc, char const *argv[])
 {
 	int cant;
 	char datos[10];
 	cant=cargar(datos,10);
-	
+	busqueda(cant);
 }
 /*
 FUNCIONES
 */
-int cargar(int datos[], int dimension)
+int cargar(char datos[], int dimension)
 {
 	int i = 0;
 	char control = 's';
@@ -22,20 +24,31 @@ int cargar(int datos[], int dimension)
 		fflush(stdin);
 		scanf("%c",&control);
 		i++;
-		printf("cantidad: %d \n", i);
 	} while (i < dimension && control =='s');
 	return i;
 }
 //
 //
-int busqueda(int cant, char datos[])
+void busqueda(int cant, char datos[])
 {
-	int i;
+	int i,rbusq;
+	char busq;
+	printf("Ingrese el caracter que desea buscar:\n");
+	fflush(stdin);
+	scanf("%c";&busq);
 	for ( i = 0; i < cant; ++i)
 	{
-		if (/* condition */)
+		if (busq==datos[i])
 		{
-			/* code */
+			rbusq=1;
 		}
+	}
+	if (rbusq==1)
+	{
+		printf("El caracter buscado se encuentra en el arreglo\n");
+	}
+	else
+	{
+		printf("El caracter buscado no se encuentra en el arreglo\n");
 	}
 }
